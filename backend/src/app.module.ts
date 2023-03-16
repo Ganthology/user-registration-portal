ecea2fdb3f13b17ec861f5c0c3653acd7eb55526
Ray Gan Boon Kit <ray.ganbk@gmail.com>
2023-03-17 02:47:40 +0800

    chore: update the modules in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from 'nestjs-prisma';
+import { S3Module } from './s3/s3.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    S3Module,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

56eb3f9c8a3cbb03b9f14f549c6c434f76880733
Ray Gan Boon Kit <ray.ganbk@gmail.com>
2023-03-17 21:59:01 +0800

    chore: frontend setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { MantineProvider } from '@mantine/core';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/login';
+import UserList from './routes/user-list';
+import UserProfile from './routes/user-profile';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Login />,
+  },
+  {
+    path: '/user/list',
+    element: <UserList />,
+  },
+  {
+    path: '/user/profile',
+    element: <UserProfile />,
+  },
+]);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withGlobalStyles>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
